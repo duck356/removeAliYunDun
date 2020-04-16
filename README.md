6b430858fcacef770e4b8d50025a3c9f40424205
356 <youngjie999@gmail.com>
2020-04-16 16:12:21 +0800

    更新屏蔽ip列表

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-
-
 ```shell
 wget --no-check-certificate https://raw.githubusercontent.com/duck356/removeAliYunDun/master/removeyundun.sh
 chmod +x removeyundun.sh
@@ -7,4 +5,7 @@ chmod +x removeyundun.sh
 ```
 
 #### 删除阿里云盾
+
+#### 20200406更新屏蔽ip列表
+
 =======
--- removeyundun.sh
+++ removeyundun.sh
@@ -48,17 +48,6 @@ iptables -I INPUT -s 140.205.225.206/32 -j DROP
 iptables -I INPUT -s 140.205.225.205/32 -j DROP
 iptables -I INPUT -s 140.205.225.195/32 -j DROP
 iptables -I INPUT -s 140.205.225.204/32 -j DROP
-iptables -I INPUT -s 140.205.201.0/28 -j DROP
-iptables -I INPUT -s 140.205.201.16/29 -j DROP
-iptables -I INPUT -s 140.205.201.32/28 -j DROP
-iptables -I INPUT -s 140.205.225.192/29 -j DROP
-iptables -I INPUT -s 140.205.225.200/30 -j DROP
-iptables -I INPUT -s 140.205.225.184/29 -j DROP
-iptables -I INPUT -s 140.205.225.183/32 -j DROP
-iptables -I INPUT -s 140.205.225.206/32 -j DROP
-iptables -I INPUT -s 140.205.225.205/32 -j DROP
-iptables -I INPUT -s 140.205.225.195/32 -j DROP
-iptables -I INPUT -s 140.205.225.204/32 -j DROP
 iptables -I INPUT -s 106.11.224.0/26 -j DROP
 iptables -I INPUT -s 106.11.224.64/26 -j DROP
 iptables -I INPUT -s 106.11.224.128/26 -j DROP
